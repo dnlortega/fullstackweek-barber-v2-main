0ad350b094b13ce73686ca16455eb22065cf02e0
dnlortega <dnlortega@hotmail.com>
2025-06-24 22:01:15 -0300

    fix: update footer component to display the current year and correct copyright name

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,12 +1,15 @@
 import { Card, CardContent } from "./ui/card"
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear() // Obtém o ano atual
+
   return (
     <footer>
       <Card>
         <CardContent className="px-5 py-6">
           <p className="text-sm text-gray-400">
-            © 2023 Copyright <span className="font-bold">FSW Barber</span>
+            © {currentYear} Copyright{" "}
+            <span className="font-bold">Daniel Ortega Pereira</span>
           </p>
         </CardContent>
       </Card>
